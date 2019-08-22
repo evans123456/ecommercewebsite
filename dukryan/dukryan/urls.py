5b063e2b0244292bd63dab4e5e567e4bd230d322
evans123456 <evanpersie3@gmail.com>
2019-08-22 18:39:38 +0300

    first commit including auth and assets

---
+++ dukryan/dukryan/urls.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from django.conf.urls import url, include
+from django.urls import path
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    url(r'^accounts/', include('allauth.urls')),
+
+]
---
+++ myenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/voldemort/mastuff/dukryan/myenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
